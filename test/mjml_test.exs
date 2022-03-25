e65450a6210d319108bf1eb1429f797d1fa41cab
Paul Götze <paul.christoph.goetze@gmail.com>
2022-03-25 19:29:05 +0100

    Use similar test mjml as the mjml.io try-live example

--- test/mjml_test.exs
+++ test/mjml_test.exs
@@ -2,9 +2,26 @@ defmodule MjmlTest do
   use ExUnit.Case
 
   test "transpiles the given MJML to HTML" do
-    mjml = "<mjml></mjml>"
+    mjml = """
+      <mjml>
+        <mj-body>
+          <mj-section>
+            <mj-column>
+              <mj-image width="100px" src="logo.png" />
+              <mj-divider border-color="#F45E43" />
+              <mj-text font-size="20px" color="#F45E43" font-family="helvetica">
+                Hello<br>Email!
+              </mj-text>
+            </mj-column>
+          </mj-section>
+        </mj-body>
+      </mjml>
+    """
+
     assert {:ok, html} = Mjml.to_html(mjml)
     assert String.starts_with?(html, "<!doctype html>")
+    assert String.ends_with?(html, "</html>")
+    assert html =~ "Hello<br />Email!"
   end
 
   test "fails to parse invalid MJML" do
