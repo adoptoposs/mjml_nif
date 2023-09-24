29607f19c96b37432790cbb658911bdbf2cda6c6
Paul Götze <paul.christoph.goetze@gmail.com>
2023-09-24 13:15:07 +0200

    Drop Support for Elixir < v1.12
    This is required by rustler_precompiled v0.7.0

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Mjml.MixProject do
     [
       app: :mjml,
       version: @version,
-      elixir: "~> 1.11",
+      elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       compilers: Mix.compilers(),
       name: "mjml",
