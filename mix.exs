821f096f9cba0a9737ec3750ba4b944ffb02d5a7
Paul Götze <paul.christoph.goetze@gmail.com>
2023-10-15 15:33:24 +0200

    Drop support for Elixir v1.12

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Mjml.MixProject do
     [
       app: :mjml,
       version: @version,
-      elixir: "~> 1.12",
+      elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       compilers: Mix.compilers(),
       name: "mjml",
